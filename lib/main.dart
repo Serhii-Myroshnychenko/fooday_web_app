95ba02626b863fe95c5de64c38891ba47374f507
Serhii-Myroshnychenko <serhii.myroshnychenko@nure.ua>
2021-04-29 16:15:18 +0300

    Update main.dart
    new AppBar(edited header)

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FooDay',
+
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'FooDay'),
     );
   }
 }
@@ -68,11 +61,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200.0),
+          child: AppBar(
+            title: Text(widget.title),
+            shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.vertical(
+                bottom: new Radius.elliptical(MediaQuery.of(context).size.width, 100.0)
+              )
+            ),
+          )
       ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
